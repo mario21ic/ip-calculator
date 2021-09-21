b3ca3065850a852cc9e91041fffd7b2d267386a8
Mario Inga <mario21ic@gmail.com>
2021-09-21 04:14:39 -0500

    feat(global) adding subnets with tables

--- lib/rango.py
+++ lib/rango.py
@@ -1,5 +1,6 @@
 import copy
 from math import log as logaritmo
+from math import ceil
 
 from .convert import dec_bin, bin_dec, ip_bin, fill_last_n
 from .mask import group_by8
@@ -13,6 +14,10 @@ def get_ips_n(my_prefix, with_net_broadcast = True):
         return 2**get_n(my_prefix)
     return (2**get_n(my_prefix))-2
 
+# Formula: 2 ^ (n + 2) (inico y fin) >= cantidad de direcciones posibles IPs
+def getN(ip_qty):
+    return ceil(logaritmo(ip_qty+2, 2))
+
 # Formula: 2^n >= cantidad de direcciones IP requeridas
 def classless_porcion_host(ip_qty):
     return int(logaritmo(ip_qty, 2))
@@ -25,6 +30,24 @@ def classless_wildcard(net_mask):
     for b in net_mask.split("."):
         result.append(str(255 - int(b)))
     return ".".join(result)
+
+# Convertir a binario, mantener los n_prefix primeros y rellenar los demas con 0
+def get_subnet_direccion_red(my_ip, n_prefix):
+    #print("n_prefix:", n_prefix)
+    bits = ip_bin(my_ip)
+    #print("bits", bits)
+
+    bits = fill_last_n(bits, (32 - n_prefix), "0")
+    #print("bits", bits)
+
+    blocks = group_by8(bits)
+    #print("blocks", blocks)
+    result = []
+    for b in blocks:
+        result.append(str(bin_dec(b)))
+    #print("result", result)
+    return ".".join(result)
+
 
 # Convertir a binario y rellenar los ultimos n con 0 de la IP
 def get_direccion_red(my_ip, n):
@@ -41,6 +64,24 @@ def get_direccion_red(my_ip, n):
         result.append(str(bin_dec(b)))
     #print("result", result)
     return ".".join(result)
+
+
+# A la direccion de red sumarle el wildcard
+def get_subnet_direccion_broadcast(direccion_red, wildcard):
+    direccion_red_split = direccion_red.split(".")
+    wildcard_split = wildcard.split(".")
+    #print("direccion_red_split:", direccion_red_split)
+    #print("wildcard_split:", wildcard_split)
+
+    results = []
+    x = 0
+    for block in direccion_red_split:
+        results.append( str( int(block) + int(wildcard_split[x]) ) )
+        x += 1
+
+    #print("results", results)
+    return ".".join(results)
+
 
 # Convertir a binario y rellenar los ultimos n con 1 de la IP
 def get_direccion_broadcast(my_ip, n):
---
+++ subnets-table.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Usage:   ./subnets.py ip prefix
+# Example: ./subnets.py 192.168.10.15 20
+
+import sys
+
+from lib.clase import get_clase
+from lib.rango import get_rango, get_direccion_red, get_direccion_broadcast, get_primera_direccion, get_ultima_direccion, classless_porcion_host, classless_porcion_red, classless_wildcard, getN, get_subnet_direccion_red, get_subnet_direccion_broadcast
+from lib.net_host import get_net_host, get_ip_qty
+from lib.mask import get_mask
+
+
+my_ip = sys.argv[1]
+my_prefix = int(sys.argv[2])
+ips_requeridas = int(sys.argv[3])
+
+#porcion_host = 32 - porcion_red # TODO
+porcion_host = getN(ips_requeridas)
+porcion_red = 32 - porcion_host
+ip_qty = get_ip_qty(porcion_host)
+
+print("Porcion de Host: %s" % porcion_host)
+print("Porcion de Red: 32 - %s = %s" % (porcion_host, porcion_red))
+print("Prefix: /%s" % porcion_red)
+print("IPs: 2^%s = %s" % (porcion_host, ip_qty))
+
+print("="*6)
+
+my_mask = get_mask(porcion_red)
+print("Mask:", my_mask)
+wildcard = classless_wildcard(my_mask)
+print("Wildcard:", wildcard)
+
+print("="*6)
+
+direccion_red = get_subnet_direccion_red(my_ip, my_prefix)
+print("direccion de red:", direccion_red)
+direccion_broadcast = get_subnet_direccion_broadcast(direccion_red, wildcard)
+print("direccion de broadcast:", direccion_broadcast)
+
+print("="*6)
+
+print("primera direccion disponible:", get_primera_direccion(direccion_red))
+print("ultima direccion disponible:", get_ultima_direccion(direccion_broadcast))
+
+"""
+Notes:
+Inspirado en https://mxtoolbox.com/subnetcalculator.aspx
+
+TODO:
+- Agregar tabla comenzando por el que requiere mas IPs (clase 7_1 1:02)
+- Refactor and unit tests
+- Direccion ip, mask y subred en binario
+"""
+
